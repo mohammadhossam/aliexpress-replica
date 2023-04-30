0fa9f275d4badf5333735cb4a4fd812d2eec1cf6
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2023-04-30 22:14:38 +0300

    init commit

---
+++ users-service/src/main/java/com/aliexpress/usersservice/UsersServiceApplication.java
@@ -0,0 +1,12 @@
+package com.aliexpress.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+}
