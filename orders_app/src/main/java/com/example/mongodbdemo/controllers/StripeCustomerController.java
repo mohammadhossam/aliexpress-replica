c258dc642e1cde7e064edb7b9ee3b5887681b4fb
Iman <iman.osama2000@gmail.com>
2023-05-06 14:00:37 +0300

    started payment

---
+++ orders_app/src/main/java/com/example/mongodbdemo/controllers/StripeCustomerController.java
@@ -0,0 +1,24 @@
+package com.example.mongodbdemo.controllers;
+import com.example.mongodbdemo.dtos.CustomerRequest;
+import com.example.mongodbdemo.services.StripeService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StripeCustomerController {
+
+    @Autowired
+    private StripeService stripeService;
+
+    @PostMapping("/customers")
+    public Customer createCustomer(@RequestBody CustomerRequest request) throws StripeException {
+        String email = request.getEmail();
+        String source = request.getSource();
+
+        return stripeService.createCustomer(email, source);
+    }
+}
