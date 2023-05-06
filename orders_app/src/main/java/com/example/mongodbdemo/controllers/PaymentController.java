cac9dd3c7b232247d56421722f2af67ab92fa6a4
Iman <iman.osama2000@gmail.com>
2023-05-06 15:24:06 +0300

    refund by stripe charge id

--- orders_app/src/main/java/com/example/mongodbdemo/controllers/PaymentController.java
+++ orders_app/src/main/java/com/example/mongodbdemo/controllers/PaymentController.java
@@ -3,6 +3,7 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
 import com.stripe.model.Customer;
+import com.stripe.model.Refund;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 import com.example.mongodbdemo.dtos.PaymentRequest;
+import com.example.mongodbdemo.dtos.RefundRequest;
 
 @RestController
 public class PaymentController {
     @Value("${stripe.api.key}")
     private String stripeApiKey;
 
-    @PostMapping("/charge")
+    @PostMapping("/charges")
     public ResponseEntity<?> chargeCard(@RequestBody PaymentRequest paymentRequest) {
         try {
             // Set your Stripe API key
@@ -45,6 +47,27 @@ public class PaymentController {
 
             // Return a success response
             return ResponseEntity.ok(charge.getId());
+        } catch (StripeException e) {
+            // Return an error response
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+    /*refund by charge id - todo we should link charge id with the order id (orders DB)
+                              + update associated merchants accounts (which DB?) */
+    @PostMapping("/refunds")
+    public ResponseEntity<?> refundCharge(@RequestBody RefundRequest refundRequest) {
+        try {
+            // Set your Stripe API key
+            Stripe.apiKey = stripeApiKey;
+
+            String chargeId = refundRequest.getChargeId();
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("charge", chargeId);
+            Refund refund = Refund.create(params);
+
+            // Return a success response
+            return ResponseEntity.ok(refund.getId());
         } catch (StripeException e) {
             // Return an error response
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
