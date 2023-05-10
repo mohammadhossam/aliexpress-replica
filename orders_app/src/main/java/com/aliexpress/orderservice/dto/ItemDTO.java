88590b81daf5b251c5edef1cce2968e03ca49a4d
Nada Abdo <52209618+Nadaabdo588@users.noreply.github.com>
2023-05-10 19:46:45 +0300

    Added order and item dtos

---
+++ orders_app/src/main/java/com/aliexpress/orderservice/dto/ItemDTO.java
@@ -0,0 +1,15 @@
+package com.aliexpress.orderservice.dto;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDTO {
+
+    private String id;
+    private double price;
+    private Integer quantity;
+}
