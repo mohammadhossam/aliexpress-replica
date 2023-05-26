6586e7c51b74ab9968036657348fe71555225cb3
Nada Abdo <52209618+Nadaabdo588@users.noreply.github.com>
2023-05-26 22:46:09 +0300

    feat(cache): cashing users' orders added

--- orders_app/src/main/java/com/aliexpress/orderservice/models/Order.java
+++ orders_app/src/main/java/com/aliexpress/orderservice/models/Order.java
@@ -1,10 +1,13 @@
 package com.aliexpress.orderservice.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Sharded;
+import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -14,7 +17,8 @@ import java.util.UUID;
 @Sharded(shardKey = { "user_id" })
 @AllArgsConstructor
 @NoArgsConstructor
-public class Order {
+@Data
+public class Order implements Serializable{
     @Id
     private String id;
     private String user_id;
