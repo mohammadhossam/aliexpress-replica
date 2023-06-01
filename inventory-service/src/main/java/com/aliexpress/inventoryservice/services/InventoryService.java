e13d48551864179c8fe71d9bc03f3f5661f67d45
kbadrawy <khadija.badrawy@gmail.com>
2023-06-01 12:43:27 +0200

    refactor(transactions): Added commands for the message queues (2/2)

--- inventory-service/src/main/java/com/aliexpress/inventoryservice/services/InventoryService.java
+++ inventory-service/src/main/java/com/aliexpress/inventoryservice/services/InventoryService.java
@@ -135,17 +135,36 @@ public class InventoryService {
 
     }
 
+
     public void sendJsonMessage(Message message, String exchangeName, String jsonRoutingKey) {
         logger.info(String.format("Sent JSON message => %s", message.toString()));
         rabbitTemplate.convertAndSend(exchangeName, jsonRoutingKey, message);
     }
     public void sendJsonMessage(OrderResponse orderResponse, String exchangeName, String jsonRoutingKey) {
-        logger.info(String.format("Sent JSON message => %s", orderResponse.toString()));
-        rabbitTemplate.convertAndSend(exchangeName, jsonRoutingKey, orderResponse);
+//        logger.info(String.format("Sent JSON message => %s", orderResponse.toString()));
+//        rabbitTemplate.convertAndSend(exchangeName, jsonRoutingKey, orderResponse);
+        try {
+            logger.info(String.format("Sent JSON message => %s", orderResponse.toString()));
+            String json = objectMapper.writeValueAsString(orderResponse);
+
+            HashMap<String, Object> dataMap = new HashMap<>();
+            dataMap.put("OrderResponse", json);
+            Message message = Message.builder()
+                    .messageId(UUID.randomUUID().toString())
+                    .routingKey(jsonRoutingKey)
+                    .messageDate(new Date())
+                    .command(CommandEnum.DeleteOrderCommand)
+                    .source("inventory")
+                    .dataMap(dataMap)
+                    .exchange(exchangeName)
+                    .build();
+            rabbitTemplate.convertAndSend(exchangeName, jsonRoutingKey, message);
+        } catch (Exception e) {
+            logger.info("Error " + e.getMessage());
+        }
     }
 
-    @RabbitListener(queues = {"${rabbitmq.jsonQueuePayToInv.name}"})
-    public void orderRollback(OrderResponse orderResponse) {
+    public void rollbackOrder(OrderResponse orderResponse) {
         incrementProducts(mapItemsDTOToIDs(orderResponse.getItems()),
                 mapItemsDTOToAmounts(orderResponse.getItems()));
         sendJsonMessage(orderResponse, exchangeNameInvToOrd, jsonRoutingKeyInvToOrd);
