30144eb89717a7aba62206eda7509fc1743d3ced
ahmedhossam <ahmed.hosssam.elden@gmail.com>
2023-06-02 17:38:45 +0300

    feat(product): make ProductResponse Serializable

--- product-service/src/main/java/com/msa/dto/ProductResponse.java
+++ product-service/src/main/java/com/msa/dto/ProductResponse.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductResponse {
+public class ProductResponse implements Serializable {
     private String id;
     private String name;
     private String description;
